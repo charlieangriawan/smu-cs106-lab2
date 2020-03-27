16a14cf02ded3440c7828ed0d63767358c1c6a0b
charlieangriawan <charlieangriawan@hotmail.com>
2020-03-27 23:42:47 +0800

    Add simple radio send/receive event

--- challenge1/source/gateway/main.cpp
+++ challenge1/source/gateway/main.cpp
@@ -1,12 +1,31 @@
 #include "MicroBit.h"
 
+// Microbit global variables
+int MICROBIT_SLEEP_INTERVAL = 10;
+
+// Radio communications
+#define DEVICE_WEARABLE         4000
+#define WEARABLE_EVT_HELP_ON    1
+
+void wearable_onhelpRequest(MicroBitEvent);
+
 MicroBit uBit;
 
 int main()
 {
     uBit.init();
 
-    uBit.display.scroll("GATEWAY!");
+    uBit.radio.enable();
 
-    release_fiber();
+    uBit.messageBus.listen(DEVICE_WEARABLE, WEARABLE_EVT_HELP_ON, wearable_onhelpRequest);
+
+    uBit.display.print("G");
+    while (true) {
+        uBit.sleep(MICROBIT_SLEEP_INTERVAL);
+    }
+}
+
+void wearable_onhelpRequest(MicroBitEvent e)
+{
+    uBit.serial.printf("help requested ");
 }
--- challenge1/source/wearable/main.cpp
+++ challenge1/source/wearable/main.cpp
@@ -1,12 +1,31 @@
 #include "MicroBit.h"
 
+// Microbit global variables
+int MICROBIT_SLEEP_INTERVAL = 10;
+
+// Radio communications
+#define DEVICE_WEARABLE         4000
+#define WEARABLE_EVT_HELP_ON    1
+
+void buttonA_onDown(MicroBitEvent);
+
 MicroBit uBit;
 
 int main()
 {
     uBit.init();
 
-    uBit.display.scroll("WEARABLE!");
+    uBit.radio.enable();
+    uBit.radio.event.listen(DEVICE_WEARABLE, MICROBIT_EVT_ANY);
 
-    release_fiber();
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_DOWN, buttonA_onDown);
+
+    uBit.display.print("W");
+    while (true) {
+        uBit.sleep(MICROBIT_SLEEP_INTERVAL);
+    }
+}
+
+void buttonA_onDown(MicroBitEvent e) {
+    MicroBitEvent(DEVICE_WEARABLE, WEARABLE_EVT_HELP_ON);
 }
