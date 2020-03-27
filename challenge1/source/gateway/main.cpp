d949128efc1eed9bfc9e7b6ba70bc8a28e5825d7
charlieangriawan <charlieangriawan@hotmail.com>
2020-03-28 03:26:24 +0800

    Add button_b and motion detection implementation

--- challenge1/source/gateway/main.cpp
+++ challenge1/source/gateway/main.cpp
@@ -4,10 +4,14 @@
 int MICROBIT_SLEEP_INTERVAL = 10;
 
 // Radio communications
-#define DEVICE_WEARABLE         4000
-#define WEARABLE_EVT_HELP_ON    1
+#define DEVICE_WEARABLE                 9701
+#define WEARABLE_EVT_HELP_ON            1
+#define WEARABLE_EVT_HELP_OFF           2
+#define WEARABLE_EVT_SHAKE_DETECTED     3
 
-void wearable_onhelpRequest(MicroBitEvent);
+void wearable_onHelpRequest(MicroBitEvent);
+void wearable_onHelpCancelation(MicroBitEvent);
+void wearable_onMotionDetected(MicroBitEvent);
 
 MicroBit uBit;
 
@@ -17,7 +21,9 @@ int main()
 
     uBit.radio.enable();
 
-    uBit.messageBus.listen(DEVICE_WEARABLE, WEARABLE_EVT_HELP_ON, wearable_onhelpRequest);
+    uBit.messageBus.listen(DEVICE_WEARABLE, WEARABLE_EVT_HELP_ON, wearable_onHelpRequest);
+    uBit.messageBus.listen(DEVICE_WEARABLE, WEARABLE_EVT_HELP_OFF, wearable_onHelpCancelation);
+    uBit.messageBus.listen(DEVICE_WEARABLE, WEARABLE_EVT_SHAKE_DETECTED, wearable_onMotionDetected);
 
     uBit.display.print("G");
     while (true) {
@@ -25,7 +31,14 @@ int main()
     }
 }
 
-void wearable_onhelpRequest(MicroBitEvent e)
-{
+void wearable_onHelpRequest(MicroBitEvent e) {
     uBit.serial.printf("help requested ");
+}
+
+void wearable_onHelpCancelation(MicroBitEvent e) {
+    uBit.serial.printf("help request canceled ");
+}
+
+void wearable_onMotionDetected(MicroBitEvent e) {
+    uBit.serial.printf("motion detected ");
 }
--- challenge1/source/wearable/main.cpp
+++ challenge1/source/wearable/main.cpp
@@ -4,10 +4,14 @@
 int MICROBIT_SLEEP_INTERVAL = 10;
 
 // Radio communications
-#define DEVICE_WEARABLE         4000
-#define WEARABLE_EVT_HELP_ON    1
+#define DEVICE_WEARABLE                 9701
+#define WEARABLE_EVT_HELP_ON            1
+#define WEARABLE_EVT_HELP_OFF           2
+#define WEARABLE_EVT_SHAKE_DETECTED     3
 
 void buttonA_onDown(MicroBitEvent);
+void buttonB_onDown(MicroBitEvent);
+void accelerometer_onShake(MicroBitEvent);
 
 MicroBit uBit;
 
@@ -19,6 +23,8 @@ int main()
     uBit.radio.event.listen(DEVICE_WEARABLE, MICROBIT_EVT_ANY);
 
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_DOWN, buttonA_onDown);
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_DOWN, buttonB_onDown);
+    uBit.messageBus.listen(MICROBIT_ID_GESTURE, MICROBIT_ACCELEROMETER_EVT_SHAKE, accelerometer_onShake);
 
     uBit.display.print("W");
     while (true) {
@@ -28,4 +34,12 @@ int main()
 
 void buttonA_onDown(MicroBitEvent e) {
     MicroBitEvent(DEVICE_WEARABLE, WEARABLE_EVT_HELP_ON);
+}
+
+void buttonB_onDown(MicroBitEvent e) {
+    MicroBitEvent(DEVICE_WEARABLE, WEARABLE_EVT_HELP_OFF);
+}
+
+void accelerometer_onShake(MicroBitEvent e) {
+    MicroBitEvent(DEVICE_WEARABLE, WEARABLE_EVT_SHAKE_DETECTED);
 }
