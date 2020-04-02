c745ee2d8238ce64cd3cd66e7320d8a578f4f5f7
charlieangriawan <charlieangriawan@hotmail.com>
2020-04-02 17:52:04 +0800

    Implement challenge 2 for multiple uBits

--- challenge2/source/leader/main.cpp
+++ challenge2/source/leader/main.cpp
@@ -1,10 +1,13 @@
 #include "MicroBit.h"
 
-// Microbit final global variables
+// MicroBit final global variables
 int MICROBIT_SLEEP_INTERVAL = 1000;
 
-int PREV_TOTAL_RATE_OF_CHANGE = 0;
 int TOTAL_RATE_OF_CHANGE = 0;
+int SIGNALER_MEMBER_COUNT = 0;
+
+MicroBitImage ICON_BOX("255,255,255,255,255\n255,255,255,255,255\n255,255,255,255,255\n255,255,255,255,255\n255,255,255,255,255\n");
+
 
 MicroBit uBit;
 
@@ -16,33 +19,35 @@ int main()
 
     uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onDataChannel2);
 
-    uBit.radio.setGroup(2);
+    uBit.radio.setGroup(202);
 
     uBit.radio.enable();
 
     uBit.display.print("L");
 
     while (true) {
-        PREV_TOTAL_RATE_OF_CHANGE = TOTAL_RATE_OF_CHANGE;
+        SIGNALER_MEMBER_COUNT = 0;
         TOTAL_RATE_OF_CHANGE = 0;
+
         uBit.sleep(MICROBIT_SLEEP_INTERVAL);
 
-        int DIFF_RATE_OF_CHANGE = PREV_TOTAL_RATE_OF_CHANGE - TOTAL_RATE_OF_CHANGE;
-        // uBit.serial.printf("%d \r\n", DIFF_RATE_OF_CHANGE);
-        if (DIFF_RATE_OF_CHANGE > 100 || DIFF_RATE_OF_CHANGE < -100) {
-            uBit.serial.printf("%s \r\n", "you move");
+        // uBit.serial.printf("FLUX:%d %d \r\n",
+        //         TOTAL_RATE_OF_CHANGE / SIGNALER_MEMBER_COUNT,
+        //         SIGNALER_MEMBER_COUNT);
+        if (TOTAL_RATE_OF_CHANGE / SIGNALER_MEMBER_COUNT >= 10) {
+            uBit.display.print(ICON_BOX);
         } else {
-            uBit.serial.printf("%s \r\n", "you no move");
+            uBit.display.print("");
         }
     }
 }
 
 void onDataChannel2(MicroBitEvent) {
     PacketBuffer buffer = uBit.radio.datagram.recv();
-    // signal strength range: 0 - 86
-    // int signalStrength =  128 + buffer.getRSSI();
+    // Signal Strength range: 0 - 86
     int rateOfChange = buffer[0];
-    // uBit.serial.printf("%d \r\n", rateOfChange);
-
     TOTAL_RATE_OF_CHANGE += rateOfChange;
+    SIGNALER_MEMBER_COUNT++;
+    // uBit.serial.printf("%d:%d \r\n", SIGNALER_MEMBER_COUNT, rateOfChange);
+
 }
--- challenge2/source/member/main.cpp
+++ challenge2/source/member/main.cpp
@@ -1,10 +1,15 @@
 #include "MicroBit.h"
 
-// Microbit final global variables
-int MICROBIT_SLEEP_INTERVAL = 200;
+// uBit final global variables
+int const MICROBIT_SLEEP_INTERVAL = 500;
 
-int SIGNALER_MEMBER_COUNT = 0;
 int TOTAL_SIGNAL_STRENGTH = 0;
+int SIGNALER_MEMBER_COUNT = 0;
+
+int PREV_AVG_SIGNAL_STRENGTH = 0;
+
+int RADIO_CHANNEL_1 = 101;
+int RADIO_CHANNEL_2 = 202;
 
 void onDataChannel1(MicroBitEvent);
 
@@ -12,6 +17,8 @@ void broadcastBaseSignal();
 void sendFluxData();
 
 MicroBit uBit;
+
+int INTERVAL_ID = 0;
 
 int main()
 {
@@ -22,7 +29,7 @@ int main()
     uBit.radio.enable();
     uBit.radio.setTransmitPower(7);
     uBit.radio.setFrequencyBand(100);
-    uBit.radio.setGroup(1);
+    uBit.radio.setGroup(RADIO_CHANNEL_1);
 
     uBit.display.print("M");
 
@@ -34,32 +41,48 @@ int main()
 
         broadcastBaseSignal();
         sendFluxData();
+
+        INTERVAL_ID++;
     }
 }
 
 void onDataChannel1(MicroBitEvent) {
     PacketBuffer buffer = uBit.radio.datagram.recv();
+    // getRSSI range: -128 - -42
     // signal strength range: 0 - 86
     int signalStrength =  128 + buffer.getRSSI();
 
-    uBit.serial.printf("%d \r\n", signalStrength);
-
     TOTAL_SIGNAL_STRENGTH += signalStrength;
     SIGNALER_MEMBER_COUNT++;
+
+    // uBit.serial.printf("%d\r\n", SIGNALER_MEMBER_COUNT);
 }
 
 void broadcastBaseSignal() {
+
     PacketBuffer buffer(1);
+
     buffer[0] = 0; 
     uBit.radio.datagram.send(buffer);
 }
 
 void sendFluxData() {
-    uBit.radio.setGroup(2);
+    uBit.radio.setGroup(RADIO_CHANNEL_2);
 
     PacketBuffer buffer(1);
-    buffer[0] = TOTAL_SIGNAL_STRENGTH/SIGNALER_MEMBER_COUNT; 
+
+    // increase precision of signal strength
+    int averageSignalStrength = TOTAL_SIGNAL_STRENGTH * 10  / SIGNALER_MEMBER_COUNT;
+    // int averageSignalStrength = TOTAL_SIGNAL_STRENGTH * 1000 / SIGNALER_MEMBER_COUNT;
+    int flux = abs(averageSignalStrength - PREV_AVG_SIGNAL_STRENGTH);
+    buffer[0] =  flux;
+
+    uBit.serial.printf("%d\r\n", flux);
+
     uBit.radio.datagram.send(buffer);
 
-    uBit.radio.setGroup(1);
+    PREV_AVG_SIGNAL_STRENGTH = averageSignalStrength;
+
+    // reset uBit to channel 1 for broadcast and listen of channel 1 (base signal)
+    uBit.radio.setGroup(RADIO_CHANNEL_1);
 }
